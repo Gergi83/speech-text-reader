2411332ac7130e7d90acded78d7b212349822c47
Gergi83 <georgiyordanov@protonmail.com>
2021-04-09 18:29:45 +0300

    final additions

--- script.js
+++ script.js
@@ -72,5 +72,70 @@ function createBox(item) {
     <p class="info">${text}</p>
   `;
 
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
+
+    // add active effect
+    box.classList.add('active');
+    setTimeout(() => box.classList.remove('active'), 800);
+  });
+
   main.appendChild(box);
 }
+
+// init speech synth
+const message = new SpeechSynthesisUtterance();
+
+// store voices
+let voices = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+  voices.forEach((voice) => {
+    const option = document.createElement('option');
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+// set text
+function setTextMessage(text) {
+  message.text = text;
+}
+
+// speak text
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+// set voice
+function setVoice(e) {
+  message.voice = voices.find((voice) => voice.name === e.target.value);
+}
+
+// voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+// toggle text box
+toggleBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.toggle('show')
+);
+
+// close btn
+closeBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.remove('show')
+);
+
+// change voice
+voicesSelect.addEventListener('change', setVoice);
+
+// read btn
+readBtn.addEventListener('click', () => {
+  setTextMessage(textArea.value);
+  speakText();
+});
+
+getVoices();
